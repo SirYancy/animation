5bc791132080725ea8d37de521acb02a16399970
Eric Kuha <erickuha@gmail.com>
2018-11-30 09:23:52 -0600

    committing

--- collada/colladaloader.cpp
+++ collada/colladaloader.cpp
@@ -7,9 +7,9 @@
 
 char data_types[4][15] = {"float_array", "int_array", "bool_array", "Name_array"};
 
-ColladaLoader::ColladaLoader(std::string filename) : filename(filename){}
+ColladaLoader::ColladaLoader(std::string filename) : filename(filename) {}
 
-void ColladaLoader::ReadGeometries(std::vector<Geometry> * geometries) {
+void ColladaLoader::ReadGeometries(std::vector<Geometry> *geometries) {
     XMLDocument doc;
     doc.LoadFile(filename.c_str());
     XMLElement *root = doc.RootElement();
@@ -20,40 +20,42 @@ void ColladaLoader::ReadGeometries(std::vector<Geometry> * geometries) {
 
     XMLElement *geometry = root->FirstChildElement("library_geometries")->FirstChildElement("geometry");
 
-    while(geometry != nullptr){
+    while (geometry != nullptr) {
         Geometry data;
         data.name = geometry->Attribute("id");
         mesh = geometry->FirstChildElement("mesh");
-        while(mesh != nullptr){
+        while (mesh != nullptr) {
             triangles = mesh->FirstChildElement("triangles");
-            if(triangles == nullptr){
+            if (triangles == nullptr) {
                 std::cerr << "We do not support the primitives in " << filename << std::endl;
                 exit(1);
             }
             input = triangles->FirstChildElement("input");
 
-            while(input != nullptr){
-                if(!strcmp(input->Attribute("semantic"), "VERTEX")){
-                    sourceName = std::string(mesh->FirstChildElement("vertices")->FirstChildElement("input")->Attribute("source"));
-                }
-                else {
+            while (input != nullptr) {
+                if (!strcmp(input->Attribute("semantic"), "VERTEX")) {
+                    sourceName = std::string(
+                            mesh->FirstChildElement("vertices")->FirstChildElement("input")->Attribute("source"));
+                } else {
                     sourceName = std::string(input->Attribute("source"));
                 }
 
-                sourceName = sourceName.erase(0,1);
+                std::cout << "" << input->Attribute("semantic") << std::endl;
+
+                sourceName = sourceName.erase(0, 1);
                 source = mesh->FirstChildElement("source");
 
-                while(source != nullptr){
-                    if(std::string(source->Attribute("id")) == sourceName){
+                while (source != nullptr) {
+                    if (std::string(source->Attribute("id")) == sourceName) {
                         data.map[std::string(input->Attribute("semantic"))] = ReadElement(source);
                     }
                     source = source->NextSiblingElement("source");
                 }
-                input = input->NextSiblingElement("input");c
+                input = input->NextSiblingElement("input");
             }
 
             primitive = mesh->FirstChildElement("triangles");
-            if(primitive == nullptr){
+            if (primitive == nullptr) {
                 std::cerr << "We do not support the primitives in " << filename << std::endl;
                 exit(1);
             }
@@ -63,11 +65,11 @@ void ColladaLoader::ReadGeometries(std::vector<Geometry> * geometries) {
             data.index_count = indicesCount;
             std::cout << "Primitives: " << primitiveCount << std::endl <<
                       "Indices: " << indicesCount << std::endl;
-            data.indices = (unsigned short*)malloc(indicesCount * sizeof(unsigned short));
-            char *text = (char*)(primitive->FirstChildElement("p")->GetText());
-            data.indices[0] = (unsigned short)atoi(strtok(text, " "));
-            for(int index=1; index<indicesCount; index++){
-                data.indices[index] = (unsigned short)atoi(strtok(NULL, " "));
+            data.indices = (unsigned short *) malloc(indicesCount * sizeof(unsigned short));
+            char *text = (char *) (primitive->FirstChildElement("p")->GetText());
+            data.indices[0] = (unsigned short) atoi(strtok(text, " "));
+            for (int index = 1; index < indicesCount; index++) {
+                data.indices[index] = (unsigned short) atoi(strtok(NULL, " "));
             }
             mesh = mesh->FirstChildElement("mesh");
         }
@@ -76,38 +78,38 @@ void ColladaLoader::ReadGeometries(std::vector<Geometry> * geometries) {
     }
 }
 
-SourceData ColladaLoader::ReadElement(XMLElement * element) {
+SourceData ColladaLoader::ReadElement(XMLElement *element) {
     SourceData sourceData;
     XMLElement *array;
     char *text;
     unsigned int numVals, stride;
     int check;
 
-    for(int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++) {
         array = element->FirstChildElement(data_types[i]);
-        if(array!=nullptr){
+        if (array != nullptr) {
             array->QueryUnsignedAttribute("count", &numVals);
             std::cout << "Values: " << numVals << std::endl;
             sourceData.size = numVals;
 
             check = element->FirstChildElement("technique_common")->FirstChildElement("accessor")
                     ->QueryUnsignedAttribute("stride", &stride);
-            if(check != XML_NO_ATTRIBUTE)
+            if (check != XML_NO_ATTRIBUTE)
                 sourceData.stride = stride;
             else
                 sourceData.stride = 1;
 
-            text = (char*)(array->GetText());
+            text = (char *) (array->GetText());
 
-            switch(i){
+            switch (i) {
                 case 0:
                     sourceData.type = GL_FLOAT;
                     sourceData.size *= sizeof(float);
                     sourceData.data = malloc(numVals * sizeof(float));
 
-                    ((float*)sourceData.data)[0] = atof(strtok(text, " "));
-                    for(unsigned int index = 1; index < numVals; index++){
-                        ((float*)sourceData.data)[index] = atof(strtok(NULL, " "));
+                    ((float *) sourceData.data)[0] = atof(strtok(text, " "));
+                    for (unsigned int index = 1; index < numVals; index++) {
+                        ((float *) sourceData.data)[index] = atof(strtok(NULL, " "));
                     }
                     break;
                 case 1:
@@ -115,9 +117,9 @@ SourceData ColladaLoader::ReadElement(XMLElement * element) {
                     sourceData.size *= sizeof(int);
                     sourceData.data = malloc(numVals * sizeof(int));
 
-                    ((int*)sourceData.data)[0] = atoi(strtok(text, " "));
-                    for(unsigned int index = 1; index < numVals; index++){
-                        ((int*)sourceData.data)[index] = atoi(strtok(NULL, " "));
+                    ((int *) sourceData.data)[0] = atoi(strtok(text, " "));
+                    for (unsigned int index = 1; index < numVals; index++) {
+                        ((int *) sourceData.data)[index] = atoi(strtok(NULL, " "));
                     }
                     break;
                 default:
@@ -129,13 +131,13 @@ SourceData ColladaLoader::ReadElement(XMLElement * element) {
     return sourceData;
 }
 
-void ColladaLoader::FreeGeometries(std::vector<Geometry> * geometries) {
+void ColladaLoader::FreeGeometries(std::vector<Geometry> *geometries) {
     std::vector<Geometry>::iterator giterator;
     SourceMap::iterator miterator;
 
-    for(giterator = geometries->begin(); giterator < geometries->end(); giterator++){
+    for (giterator = geometries->begin(); giterator < geometries->end(); giterator++) {
         free(giterator->indices);
-        for(miterator = giterator->map.begin(); miterator != giterator->map.end(); miterator++){
+        for (miterator = giterator->map.begin(); miterator != giterator->map.end(); miterator++) {
             free((*miterator).second.data);
         }
         geometries->erase(giterator);
--- main.cpp
+++ main.cpp
@@ -1,12 +1,120 @@
 #include <iostream>
+#include <fstream>
 #include "collada/colladaloader.h"
+#include "glad/glad.h"
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_opengl.h"
+
+#define GLM_FORCE_RADIANS
+
+void CreateShaderProgram(GLuint shader, GLuint fragShader);
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+
+GLuint shaderProgram;
+GLuint vao, vertexvbo, indexvbo;
+
+const char *vertfn = "../shaders/vert.glsl";
+const char *fragfn = "../shaders/frag.glsl";
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_VIDEO);
 
+    SDL_version comp;
+    SDL_version linked;
+    SDL_VERSION(&comp);
+    SDL_GetVersion(&linked);
+    printf("SDL: %d.%d.%d\n", comp.major, comp.minor, comp.patch);
+    printf("Linked: %d.%d.%d\n", linked.major, linked.minor, linked.patch);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+    SDL_Window *window = SDL_CreateWindow("Cowboy", 100, 100, 640, 480, SDL_WINDOW_OPENGL);
+
+    if(!window) {
+        printf("Window failed\n");
+        return EXIT_FAILURE;
+    }
+
+    float aspect = 640 / (float)480;
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+
+    if(gladLoadGLLoader(SDL_GL_GetProcAddress)){
+        printf("Vendor: %s\n", glGetString(GL_VENDOR));
+        printf("Renderer: %s\n", glGetString(GL_RENDERER));
+        printf("Version: %s\n", glGetString(GL_VERSION));
+    }else{
+        printf("OPENGL CONTEXT NOT LOADED\n");
+        return -1;
+    }
+
+    GLuint vertShader;
+    GLuint fragShader;
+    CreateShaderProgram(vertShader, fragShader);
+
+    printf("Loading Model\n");
     ColladaLoader *loader = new ColladaLoader("../models/cowboy.dae");
     auto *geometries = new std::vector<Geometry>();
     loader->ReadGeometries(geometries);
     loader->FreeGeometries(geometries);
     return 0;
+}
+
+int LoadShader(const char*filename, GLuint shaderID){
+    ifstream file;
+    file.open(filename, std::ios::in);
+    if (!file) return -1;
+
+    file.tellg();
+    file.seekg(0, std::ios::end);
+    int len = static_cast<int>(file.tellg());
+    file.seekg(std::ios::beg);
+
+    if (len == 0) return -1; //empty file
+
+    GLchar *shaderSource = new GLchar[len + 1];
+    shaderSource[len] = 0;
+
+    int i = 0;
+    while (file.good()) {
+        shaderSource[i] = static_cast<GLchar>(file.get());
+        if (!file.eof())
+            i++;
+    }
+
+    shaderSource[i] = 0;
+    file.close();
+
+    glShaderSource(shaderID, 1, &shaderSource, NULL);
+    glCompileShader(shaderID);
+
+    GLint status;
+    glGetShaderiv(shaderID, GL_COMPILE_STATUS, &status);
+    if (!status) {
+        char buffer[512];
+        glGetShaderInfoLog(shaderID, 512, NULL, buffer);
+        printf("Shader Compile Failed. Info:\n\n%s\n", buffer);
+        return -1;
+    }
+    return 0;
+}
+void CreateShaderProgram(GLuint *vertShader, GLuint &fragShader) {
+    vertShader = glCreateShader(GL_VERTEX_SHADER);
+    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    LoadShader(vertfn, vertShader);
+    LoadShader(fragfn, fragShader);
+
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertShader);
+    glAttachShader(shaderProgram, fragShader);
+//     glBindFragDataLocation(shaderProgram, 0, "outColor");
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+
 }
